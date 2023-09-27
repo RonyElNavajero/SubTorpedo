8fef0a9317a9287ddfd9658b7fbe6df670fe5f58
RonyElNavajero <unaidelpozo95@gmail.com>
2023-09-27 22:10:56 +0200

    SubTorpedo v1.0

--- Proyecto Pepinillo/src/main.cpp
+++ Proyecto Pepinillo/src/main.cpp
@@ -1,18 +1,91 @@
 #include <Arduino.h>
+int State = 0;
+int Switch = 22;
+int Enable = 24;
+int PWM = 2;
+int Throttle = 0;
+unsigned long t = 0;
+unsigned long t0;
+void setup() 
+{
+pinMode (PWM,OUTPUT);               //PWM Output
+pinMode (Enable,OUTPUT);              //Enable Output
+pinMode (Switch,INPUT);              //Switch Input
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() 
+{
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+switch (State) 
+{
+    case 0:                                 // Etapa Inicial
+        digitalWrite(Enable, false);
+        analogWrite(PWM,0);
+
+        if (digitalRead(Switch) == true)
+            {
+                State = 1;
+            }
+        break;
+
+    case 1:                                 // Etapa Aceleración
+        digitalWrite(Enable, true);
+        analogWrite(PWM,Throttle);
+        if (digitalRead(Switch) == true)
+            {
+                delay(500);
+                Throttle = Throttle + 40;
+                if (Throttle > 255)
+                {
+                    Throttle = 255;
+                }
+            }
+        else
+            {
+                State = 2;
+                t0 = millis();
+            }
+
+      break;
+    case 2:                                 // Etapa Espera Switch
+        digitalWrite(Enable, true);
+        analogWrite(PWM,Throttle);
+
+        if (digitalRead(Switch) == true)
+            {
+                State = 3;
+            } 
+        else
+            {
+                t = millis() - t0;
+                if (t >> 1000)
+                {
+                    State = 0;
+                }
+            }
+          
+      break;
+    case 3:                                 //Etapa Marcha Normal
+        
+        if (digitalRead(Switch) == true)
+            {
+                digitalWrite(Enable, true);
+                analogWrite(PWM,Throttle);
+                t0 = millis();
+            }
+        else
+            {
+                t = millis() - t0;
+                if (t >> 1000)
+                {
+                    State = 0;
+                }
+            } 
+
+      break;
+  default:
+    digitalWrite(Enable, false);
+    analogWrite(PWM,0);
+}
 }
