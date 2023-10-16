6d736f0f257871e14deeed5f1e6b33508cd3a73d
RonyElNavajero <unaidelpozo95@gmail.com>
2023-10-16 20:20:43 +0200

    Running With Switch and led

--- Proyecto Pepinillo/src/main.cpp
+++ Proyecto Pepinillo/src/main.cpp
@@ -1,17 +1,27 @@
 #include <Arduino.h>
 int State = 0;
-int Switch = 22;
-int Enable = 24;
-int PWM = 2;
+int Switch = 21;
+int Enable = 15;
+#define PWM 2 //PWM Pin
+// setting PWM properties
+const int freq = 5000;
+const int PWMChannel = 0;
+const int resolution = 10; //Resolution 8, 10, 12, 15
 int Throttle = 0;
 unsigned long t = 0;
 unsigned long t0;
 void setup() 
 {
-pinMode (PWM,OUTPUT);               //PWM Output
-pinMode (Enable,OUTPUT);              //Enable Output
-pinMode (Switch,INPUT);              //Switch Input
+Serial.begin(115200);
+pinMode (Enable, OUTPUT);                //Enable Output
+pinMode(PWM,OUTPUT);                    //PWM Output
+pinMode (Switch, INPUT);                 //Switch Input
 
+// configure LED PWM functionalitites
+ledcSetup(PWMChannel, freq, resolution);
+
+// attach the channel to the GPIO2 to be controlled
+ledcAttachPin(PWM, PWMChannel);
 }
 
 void loop() 
@@ -19,9 +29,11 @@ void loop()
 
 switch (State) 
 {
+    
     case 0:                                 // Etapa Inicial
         digitalWrite(Enable, false);
-        analogWrite(PWM,0);
+        ledcWrite(PWMChannel,0);
+        Throttle = 0;
 
         if (digitalRead(Switch) == true)
             {
@@ -29,55 +41,64 @@ switch (State)
             }
         break;
 
-    case 1:                                 // Etapa Aceleración
+    case 1:
         digitalWrite(Enable, true);
-        analogWrite(PWM,Throttle);
+        ledcWrite(PWMChannel,0);
+        Throttle = 0;
+        delay(500);
+        State = 2;
+
+    case 2:                                 // Etapa Aceleración
+        digitalWrite(Enable, true);
+        ledcWrite(PWMChannel,Throttle);
         if (digitalRead(Switch) == true)
             {
-                delay(500);
-                Throttle = Throttle + 40;
-                if (Throttle > 255)
+                delay(100);
+                Throttle = Throttle + 30;
+                if (Throttle > 1023)
                 {
-                    Throttle = 255;
+                    Throttle = 1023;
                 }
             }
         else
             {
-                State = 2;
-                t0 = millis();
+                State = 3;
+                t0 = esp_timer_get_time();
             }
 
       break;
-    case 2:                                 // Etapa Espera Switch
+    case 3:                                 // Etapa Espera Switch
         digitalWrite(Enable, true);
-        analogWrite(PWM,Throttle);
+        ledcWrite(PWMChannel,Throttle);
 
         if (digitalRead(Switch) == true)
             {
-                State = 3;
+                delay(100);
+                State = 4;
             } 
         else
             {
-                t = millis() - t0;
-                if (t >> 1000)
+                t = esp_timer_get_time() - t0;
+                if (t > 100000)
                 {
                     State = 0;
                 }
             }
           
       break;
-    case 3:                                 //Etapa Marcha Normal
+    case 4:                                 //Etapa Marcha Normal
         
         if (digitalRead(Switch) == true)
             {
+                delay(100);
                 digitalWrite(Enable, true);
-                analogWrite(PWM,Throttle);
-                t0 = millis();
+                ledcWrite(PWMChannel,Throttle);
+                t0 = esp_timer_get_time();
             }
         else
             {
-                t = millis() - t0;
-                if (t >> 1000)
+                t = esp_timer_get_time() - t0;
+                if (t > 100000)
                 {
                     State = 0;
                 }
@@ -86,6 +107,10 @@ switch (State)
       break;
   default:
     digitalWrite(Enable, false);
-    analogWrite(PWM,0);
+    ledcWrite(PWMChannel,0);
 }
+Serial.print(t);
+Serial.print(", ");
+Serial.println(State);
+//delay(500);
 }
