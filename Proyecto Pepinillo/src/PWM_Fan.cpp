178fb13b7d0cb712f629b7f40c3697239429fe5f
RonyElNavajero <unaidelpozo95@gmail.com>
2023-10-28 10:40:38 +0200

    PWM

--- Proyecto Pepinillo/src/PWM_Fan.cpp
+++ Proyecto Pepinillo/src/PWM_Fan.cpp
@@ -1,16 +1,19 @@
 #include <Arduino.h>
+#include "esp_timer.h"
+#include <esp32-hal-ledc.h>
 int State = 0;
-int Switch = 21;
-int Sensor = 18;
 unsigned long RPMs = 0;
-#define PWM 17 //PWM Pin
+#define Switch 21
+#define Sensor 18
+#define PWM 16 //PWM Pin
+#define Enable 5 //Enable Pin
 // setting PWM properties
-const int freq = 5000;
+const int freq = 10000;
 const int PWMChannel = 0;
 const int resolution = 10; //Resolution 8, 10, 12, 15
 int Throttle = 0;
-unsigned long t = 0;
-unsigned long t0;
+int64_t t = 0;
+int64_t t0;
 unsigned long Tick = 0;
 
 void IRAM_ATTR isr() 
@@ -20,10 +23,13 @@ void IRAM_ATTR isr()
 
 void setup() 
 {
+digitalWrite(Enable, false);
 Serial.begin(115200);
-pinMode (Sensor, INPUT);                //Sensor Input
+pinMode (Sensor, INPUT_PULLUP);                //Sensor Input
 pinMode(PWM,OUTPUT);                    //PWM Output
-pinMode (Switch, INPUT);                 //Switch Input
+pinMode(Enable,OUTPUT);                    //Enable Output
+pinMode (Switch, INPUT);               //Switch Input
+State = 0;
 
 // configure LED PWM functionalitites
 ledcSetup(PWMChannel, freq, resolution);
@@ -36,25 +42,30 @@ attachInterrupt(Sensor, isr, RISING);
 void loop() 
 {
 
-switch (State) 
+/*switch (State) 
 {
     
     case 0:                                 // Etapa Inicial
+        digitalWrite(Enable, false);
         ledcWrite(PWMChannel,0);
-        Throttle = 0;
 
         if (digitalRead(Switch) == true)
             {
                 State = 1;
                 delay(500);
+                t0 = esp_timer_get_time();
             }
         break;
 
-    case 1:
-        ledcWrite(PWMChannel,200);
-        Throttle = 0;
-        t = esp_timer_get_time() - t0;
-            if (t >= 1000000)
+    case 1:*/
+        if (Serial.available())
+        {
+            Throttle = Serial.parseInt();
+        }
+        digitalWrite(Enable, true);
+        ledcWrite(PWMChannel,Throttle);
+        t = esp_timer_get_time() ;
+            if ((t- t0) >= 1000000)
             {
                 RPMs = Tick;
                 Tick = 0;
@@ -67,11 +78,7 @@ switch (State)
                 delay(500);
             }
 
-  default:
-    ledcWrite(PWMChannel,0);
-}
-//Serial.print(t);
-//Serial.print(", ");
-//Serial.println(State);
-//delay(500);
+  //default:
+    //ledcWrite(PWMChannel,0);
+//}
 }
